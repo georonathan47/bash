bb5948ba42ae58ea987f19396ff8d0f299f26386
George Osafo-Osei Jonathan <georonathan47@gmail.com>
2023-01-03 20:02:42 +0000

    📝

--- basics/hello.sh
+++ basics/hello.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello World!"
+
+# To run any bash script, type the following in the terminal:
+# chmod +x filename.sh
+# after you have made the file executable, you can run it by typing:
+# ./filename.sh
